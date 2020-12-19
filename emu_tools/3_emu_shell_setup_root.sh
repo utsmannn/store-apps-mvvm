925163d9b3bd3e87f4c662a16197677c47ecf911
utsmannn <kucingapes@gmail.com>
2020-12-20 06:56:09 +0700

    fix shell script emulator

--- emu_tools/3_emu_shell_setup_root.sh
+++ emu_tools/3_emu_shell_setup_root.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-su root
-cd /system/bin
-chmod 06755 su
-su --install
-su --daemon&
-setenforce 0
+adb -e shell su root
+adb -e shell chmod 06755 /system/bin/su
+adb -e shell /system/bin/su --install
+adb -e shell /system/bin/su --daemon&
+adb -e shell setenforce 0
+exit
